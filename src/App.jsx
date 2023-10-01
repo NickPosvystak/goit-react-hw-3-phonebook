e7fd5eaefa272601e997e3a7c0a4f16ebd81dad3
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-10-01 19:53:42 +0200

    summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+import { Component } from "react"
+
+export class App extends Component {
+  render() {
+      return (
+    <div>
       React homework template
     </div>
   );
+  }
+
 };
---
+++ src/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '38873107-2fd59623250c411d575012e5a';
+
+export fetchImages = async () => {
+    const searchParams = new URLSearchParams({
+        key: API_KEY,
+        
+    })
+}
